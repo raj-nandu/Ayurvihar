16e89892377df300d4a818c96c13b8a1f0e42286
DevendraVyavaharkar <vyavaharkardevendra@gmail.com>
2017-03-28 00:43:14 +0530

    Added Spinner in Home Page

--- app/src/main/java/com/ayurvihar/somaiya/ayurvihar/Activity/MainActivity.java
+++ app/src/main/java/com/ayurvihar/somaiya/ayurvihar/Activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.ayurvihar.somaiya.ayurvihar.BackgroundTask;
 import com.ayurvihar.somaiya.ayurvihar.R;
@@ -35,10 +36,30 @@ public class MainActivity extends Activity {
                 }
                 else {
                     String method = "login";
+                    Spinner deptSel=(Spinner)findViewById(R.id.department_select);
+                    String dept=String.valueOf(deptSel.getSelectedItem());
+                    switch(dept)
+                    {
+                        case "Family Data":Intent intent1=new Intent(getApplicationContext(),Family.class);
+                            startActivity(intent1);
+                            break;
+                        case "NCD":Intent intent2=new Intent(getApplicationContext(),NCD.class);
+                            startActivity(intent2);
+                            break;
+                        case "Well Woman":Intent intent3=new Intent(getApplicationContext(),WellWoman.class);
+                            startActivity(intent3);
+                            break;
+                        case "Under Five":Intent intent4=new Intent(getApplicationContext(),Under5.class);
+                            startActivity(intent4);
+                            break;
+                    }
                     BackgroundTask backgroundTask = new BackgroundTask(getApplicationContext());
                     backgroundTask.execute(method, Username.getText().toString(), Password.getText().toString());
+
                 }
             }
         });
+
+
     }
 }
